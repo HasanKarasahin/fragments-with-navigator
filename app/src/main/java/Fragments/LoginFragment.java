08f9587c66903912374d3bd3dc897e3a42ce6622
HasanGaziKarasahin <hasangazikarasahin@gmail.com>
2018-12-06 17:29:04 +0300

    fragmentler ile ilgili düzenlemeler

---
+++ app/src/main/java/Fragments/LoginFragment.java
@@ -0,0 +1,24 @@
+package Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hasan.fragments_with_navigator.R;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
