0bdcff81da1956484d4b90570ffcca517cf3c882
HasanGaziKarasahin <hasangazikarasahin@gmail.com>
2018-12-07 10:03:35 +0300

    add BackFragments

---
+++ app/src/main/java/Fragments/BackGalleryFragment.java
@@ -0,0 +1,23 @@
+package Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hasan.fragments_with_navigator.R;
+
+public class BackGalleryFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_back_gallery, container, false);
+    }
+}
